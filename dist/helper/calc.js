f104ac33d48616aa1a8497f4f7ceceb9adcbf732
宮崎貴史 <>
2022-04-30 10:11:03 +0900

    tsconfig.jsonを使うように変更

--- dist/helper/calc.js
+++ dist/helper/calc.js
@@ -2,8 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.calcPriceWithTax = void 0;
 const calcPriceWithTax = (price, tax) => {
-    if (tax === 0)
-        throw Error('0は指定できません');
+    //   if (tax === 0) throw Error('0は指定できません')
     return price + price * tax;
 };
 exports.calcPriceWithTax = calcPriceWithTax;
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.spec.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
 }
